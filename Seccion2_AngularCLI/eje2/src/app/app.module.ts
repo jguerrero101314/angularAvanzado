412cf11abfe8adb5882178574647a319ccf85988
Joel Guerrero <joelg1014@hotmail.com>
2020-06-09 17:01:35 -0500

    16. Inicio de proyecto - AdminPro

--- Seccion2_AngularCLI/eje2/src/app/app.module.ts
+++ Seccion2_AngularCLI/eje2/src/app/app.module.ts
@@ -2,16 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { MainPageComponent } from './pages/main-page.component';
+import { HomeComponent } from './home2/home.componentponent';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    MainPageComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule
