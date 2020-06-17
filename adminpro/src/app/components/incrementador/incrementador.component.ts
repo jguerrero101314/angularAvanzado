1f86fee98bea9c44f4b12b0d343d44d0e7d6c498
Joel Guerrero <joelg1014@hotmail.com>
2020-06-17 16:20:06 -0500

    47. Pulir detalles de nuestro incrementador component

--- adminpro/src/app/components/incrementador/incrementador.component.ts
+++ adminpro/src/app/components/incrementador/incrementador.component.ts
@@ -3,35 +3,47 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class IncrementadorComponent implements OnInit {
+  @Input('Nombre') leyenda: string = 'Leyenda';
+  @Input() porcentaje: number = 50;
 
- @Input('Nombre') leyenda: string = 'Leyenda';
- @Input() porcentaje: number = 50;
-
- @Output('actualizaValor') cambioValor: EventEmitter<number> = new EventEmitter();
+  @Output('actualizaValor') cambioValor: EventEmitter<
+    number
+  > = new EventEmitter();
 
   constructor() {
     console.log('Leyenda: ', this.leyenda);
     console.log('Porcentaje: ', this.porcentaje);
-   }
-
-  ngOnInit(): void {
   }
-  cambiarValor(valor: number){
-    if(this.porcentaje >= 100 && valor > 0 ){
+
+  ngOnInit(): void {}
+  onChanges(newValue: number) {
+    let elemHTML: any = document.getElementsByName('porcentaje')[0];
+
+
+    if ( newValue >= 100 ) {
+      this.porcentaje = 100;
+    }else if ( newValue <= 0 ) {
+      this.porcentaje = 0;
+    }else {
+      this.porcentaje = newValue;
+    }
+    elemHTML.value = this.porcentaje;
+    this.cambioValor.emit(this.porcentaje);
+  }
+  cambiarValor(valor: number) {
+    if (this.porcentaje >= 100 && valor > 0) {
       this.porcentaje = 100;
       return;
     }
-    if(this.porcentaje <= 0 && valor < 0){
+    if (this.porcentaje <= 0 && valor < 0) {
       this.porcentaje = 0;
       return;
     }
 
     this.porcentaje = this.porcentaje + valor;
-    this.cambioValor.emit( this.porcentaje );
+    this.cambioValor.emit(this.porcentaje);
   }
-
 }
