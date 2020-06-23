c9ca64c3c4d621cba28a8b62f2add84d834273c2
Joel Guerrero <joelg1014@hotmail.com>
2020-06-23 13:57:04 -0500

    57. Agregando clases de CSS sin usar ngClass

--- adminpro/src/app/pages/account-settings/account-settings.component.ts
+++ adminpro/src/app/pages/account-settings/account-settings.component.ts
@@ -4,20 +4,25 @@ import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AccountSettingsComponent implements OnInit {
+  constructor(@Inject(DOCUMENT) private _document) {}
 
-  constructor( @Inject(DOCUMENT) private _document  ) { }
+  ngOnInit(): void {}
+  cambiarColor(tema: string, link: any) {
+    console.log(link);
 
-  ngOnInit(): void {
-  }
-  cambiarColor( tema: string ){
-    console.log(tema);
+    this.aplicarCheack(link);
 
     let url = `assets/css/colors/${tema}.css`;
-    this._document.getElementById('tema').setAttribute('href', url)
-
+    this._document.getElementById('tema').setAttribute('href', url);
+  }
+  aplicarCheack(link: any) {
+    let selectores: any = document.getElementsByClassName('selector');
+    for (let ref of selectores) {
+      ref.classList.remove('working');
+    }
+    link.classList.add('working');
   }
 }
