4881cbfbfc6c0e5f23b57d220d424078bde8d858
Joel Guerrero <joelg1014@hotmail.com>
2020-06-23 13:27:13 -0500

    56. Cambiar el CSS principal de forma dinámica

--- adminpro/src/app/pages/account-settings/account-settings.component.ts
+++ adminpro/src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-account-settings',
@@ -8,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor( @Inject(DOCUMENT) private _document  ) { }
 
   ngOnInit(): void {
   }
+  cambiarColor( tema: string ){
+    console.log(tema);
 
+    let url = `assets/css/colors/${tema}.css`;
+    this._document.getElementById('tema').setAttribute('href', url)
+
+  }
 }
