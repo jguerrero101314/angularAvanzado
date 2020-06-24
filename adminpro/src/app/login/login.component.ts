cae0ffba8b760e899704aa840f1fbf6843154212
Joel Guerrero <joelg1014@hotmail.com>
2020-06-24 09:07:21 -0500

    62. Uso de Scripts de archivos importados en el index.html en TypeScript

--- adminpro/src/app/login/login.component.ts
+++ adminpro/src/app/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+declare function init_plugins();
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor( public router: Router) {}
 
   ngOnInit(): void {
+    init_plugins();
   }
-
+  ingresar() {
+    console.log('ingresando');
+    this.router.navigate(['/dashboard']);
+  }
 }
