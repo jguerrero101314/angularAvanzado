7316c02f1c46f61f2fce7522b1d6f75363f456ec
Joel Guerrero <joelg1014@hotmail.com>
2020-07-22 09:04:19 -0700

    34. Resolución de la tarea práctica #3 - Creación de un módulo personalizado

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -5,25 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NopagefoundComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    AuthModule
   ],
   bootstrap: [AppComponent],
 })
