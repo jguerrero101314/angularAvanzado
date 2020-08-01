945665ecfad4942f6529f6b1d33fa585f9719a02
Joel Guerrero <joelg1014@hotmail.com>
2020-08-01 10:14:09 -0700

    62. Diseño inicial de la página account-settings

--- adminpro/src/app/pages/pages.routing.ts
+++ adminpro/src/app/pages/pages.routing.ts
@@ -4,6 +4,7 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { GraficasComponent } from './graficas/graficas.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 const routes: Routes = [
   {
@@ -13,7 +14,7 @@ const routes: Routes = [
       { path: '', component: DashboardComponent },
       { path: 'progress', component: ProgressComponent },
       { path: 'grafica1', component: GraficasComponent },
-     
+      { path: 'account-settings', component: AccountSettingsComponent },
     ],
   },
 ];
