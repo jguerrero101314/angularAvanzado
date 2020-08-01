28d97c9a2ae9bf9549cee6ce568d1a68f1426556
Joel Guerrero <joelg1014@hotmail.com>
2020-08-01 10:45:03 -0700

    63. Cambiar el CSS principal de forma dinámica

--- adminpro/src/app/pages/pages.component.ts
+++ adminpro/src/app/pages/pages.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styles: [],
 })
-export class PagesComponent {
-
+export class PagesComponent implements OnInit {
+  public linkTheme = document.querySelector('#theme');
 
   constructor() {}
+
+  ngOnInit(): void {
+    // ./assets/css/colors/purple-dark.css
+    const url =
+      localStorage.getItem('theme') || './assets/css/colors/purple-dark.css';
+
+    this.linkTheme.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+  }
 }
