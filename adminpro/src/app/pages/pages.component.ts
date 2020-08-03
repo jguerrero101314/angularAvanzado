f248a25565552158892ffe84319b2e45ebd6e2e6
Joel Guerrero <joelg1014@hotmail.com>
2020-08-03 08:21:05 -0700

    68. Uso de Scripts de archivos importados en el index.html en TypeScript

--- adminpro/src/app/pages/pages.component.ts
+++ adminpro/src/app/pages/pages.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+declare function  customInitFunctions();
+
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -11,5 +14,8 @@ export class PagesComponent implements OnInit {
 
   constructor( private settingsService:SettingsService ) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    customInitFunctions();
+
+   }
 }
