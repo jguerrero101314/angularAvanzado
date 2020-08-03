52f3478f3d38d752a9980e1770179613a2e0f6d0
Joel Guerrero <joelg1014@hotmail.com>
2020-08-03 07:47:27 -0700

    66. RouterLink - Mover a una ruta en particular

--- adminpro/src/app/shared/shared.module.ts
+++ adminpro/src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+
 
 
 
@@ -19,7 +22,8 @@ import { HeaderComponent } from './header/header.component';
   ],
 
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
