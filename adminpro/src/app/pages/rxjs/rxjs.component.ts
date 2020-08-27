fd399b9bdb94fb7f4d0c529510153e75bd612979
Joel Guerrero <joelg1014@hotmail.com>
2020-08-27 14:48:15 -0700

    80. Operador map de los observables

--- adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
+import { Observable, interval } from 'rxjs';
+import { retry, take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -10,14 +10,24 @@ import { retry } from 'rxjs/operators';
 export class RxjsComponent {
   constructor() {
 
-    this.retornaObservable().pipe(
+   /* this.retornaObservable().pipe(
       retry(2)
     ).subscribe(
       valor => console.log('Subs:', valor),
       error => console.warn('Error:', error),
       () => console.info('Obs terminado')
+    );*/
+    this.retornaIntervalo().subscribe(console.log);
+  }
+  retornaIntervalo(): Observable<number>{
+    return interval(1000)
+          .pipe(
+            take(4),
+            map( valor => valor + 1
+        )
     );
   }
+  
   retornaObservable(): Observable<number> {
     let i = -1;
     return new Observable<number>(observer => {
