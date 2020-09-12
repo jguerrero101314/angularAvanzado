caff1f2314429a368b521a2b921eaaf67ccf7e48
Joel Guerrero <joelg1014@hotmail.com>
2020-09-12 11:50:23 -0700

    93. Primera ruta

--- backend-server/index.js
+++ backend-server/index.js
@@ -4,6 +4,14 @@ const express = require("express");
 
 const app = express();
 
+// Rutas
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'Hola mundo'
+    });
+});
+
 app.listen(3000, () => {
     console.log('Servidor corriendo en puerto ' + 3000);
 });
