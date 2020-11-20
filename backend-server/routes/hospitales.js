7d20321dfbc5efc6ced558499f87a949775a3adb
Joel Guerrero <joelg1014@hotmail.com>
2020-11-20 09:43:03 -0800

    153. Actualizar Hospital

--- backend-server/routes/hospitales.js
+++ backend-server/routes/hospitales.js
@@ -26,7 +26,11 @@ router.post('/', [
     ],
     crearHospital
 );
-router.put('/:id', [],
+router.put('/:id', [
+        validarJWT,
+        check('nombre', 'El nombre del hospital es necesario').not().isEmpty(),
+        validarCampos
+    ],
     actualizarHospital
 );
 
