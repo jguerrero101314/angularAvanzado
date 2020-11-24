6eb5d3d5dd47e403628b572c6ab43eb502f23810
Joel Guerrero <joelg1014@hotmail.com>
2020-11-23 16:35:14 -0800

    cambios

--- adminpro/src/app/app-routing.module.ts
+++ adminpro/src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     PagesRoutingModule,
     AuthRoutingModule
   ],
