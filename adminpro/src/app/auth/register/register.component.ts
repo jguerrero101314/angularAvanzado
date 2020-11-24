5364d13f9bd12c19f39ea7ea9e4971dfa0998eb9
Joel Guerrero <joelg1014@hotmail.com>
2020-11-24 13:35:53 -0800

    162. Formulario de Registro - Reactive Forms

--- adminpro/src/app/auth/register/register.component.ts
+++ adminpro/src/app/auth/register/register.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
 
-  constructor() { }
+  public registerForm = this.fb.group({
+    nombre:['Joel',[Validators.required]],
+    email:['test01@hotmail.com',[Validators.required]],
+    password:['1222',[Validators.required]],
+    password2:['1222',[Validators.required]],
+    terminos:[false,[Validators.required]]
+  });
 
-  ngOnInit(): void {
+  constructor( private fb: FormBuilder) { }
+
+  crearUsuario(){
+    console.log(this.registerForm.value);
   }
-
 }
