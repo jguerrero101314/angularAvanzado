41ac241c35fb7f5440e54f3ee35b9de5a035bf7c
Joel Guerrero <joelg1014@hotmail.com>
2020-12-02 11:14:15 -0800

    163. Validaciones del formulario

--- adminpro/src/app/auth/register/register.component.ts
+++ adminpro/src/app/auth/register/register.component.ts
@@ -4,21 +4,34 @@ import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent  {
+export class RegisterComponent {
+  public formSubmitted = false;
 
   public registerForm = this.fb.group({
-    nombre:['Joel',[Validators.required]],
-    email:['test01@hotmail.com',[Validators.required]],
-    password:['1222',[Validators.required]],
-    password2:['1222',[Validators.required]],
-    terminos:[false,[Validators.required]]
+    nombre: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    password2: ['', [Validators.required]],
+    terminos: [false, [Validators.required]],
   });
 
-  constructor( private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
-  crearUsuario(){
+  crearUsuario() {
+    this.formSubmitted = true;
     console.log(this.registerForm.value);
+  }
+  campoNoValido(campo: string) {
+    if (this.registerForm.get(campo).invalid && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  contrasenasNoValidas(){}
+  aceptaTerminos(){
+    return !this.registerForm.get('terminos').value && this.formSubmitted;
   }
 }
