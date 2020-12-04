053e416157b5e779871dad838dbc72cbb11d31b0
Joel Guerrero <joelg1014@hotmail.com>
2020-12-03 17:24:58 -0800

    164. Validar las contraseñas iguales

--- adminpro/src/app/auth/register/register.component.ts
+++ adminpro/src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -15,6 +15,8 @@ export class RegisterComponent {
     password: ['', [Validators.required]],
     password2: ['', [Validators.required]],
     terminos: [false, [Validators.required]],
+  }, {
+    validators: this.passwordsIguales('password', 'password2')
   });
 
   constructor(private fb: FormBuilder) {}
@@ -22,6 +24,11 @@ export class RegisterComponent {
   crearUsuario() {
     this.formSubmitted = true;
     console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      console.log('posteando formulario');
+    } else {
+      console.log('Formulario no es correcto....');
+    }
   }
   campoNoValido(campo: string) {
     if (this.registerForm.get(campo).invalid && this.formSubmitted) {
@@ -30,8 +37,31 @@ export class RegisterComponent {
       return false;
     }
   }
-  contrasenasNoValidas(){}
-  aceptaTerminos(){
+  contrasenasNoValidas() {
+    const pass1 = this.registerForm.get('password').value;
+    const pass2 = this.registerForm.get('password2').value;
+    if (pass1 !== pass2 && this.formSubmitted ) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+  aceptaTerminos() {
     return !this.registerForm.get('terminos').value && this.formSubmitted;
+  }
+
+  passwordsIguales(pass1Name: string, pass2Name: string){
+
+    return (formGroup: FormGroup) => {
+        const pass1Control = formGroup.get(pass1Name);
+        const pass2Control = formGroup.get(pass2Name);
+
+        if(pass1Control.value === pass2Control.value){
+          pass2Control.setErrors(null)
+        }else{
+          pass2Control.setErrors({ noEsIgual: true})
+        }
+    }
+
   }
 }
