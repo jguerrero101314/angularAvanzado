7599d04df608d88692ff4c6b489b42d8544b6ad5
Joel Guerrero <joelg1014@hotmail.com>
2020-12-25 17:50:06 -0800

    172. Obtener el Token de una autenticación, Google Sign-In

--- adminpro/src/app/auth/login/login.component.ts
+++ adminpro/src/app/auth/login/login.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsuarioService } from '../../services/usuario.service';
-import Swal from 'sweetalert2'
-import { LoginForm } from '../../interfaces/login-form-interfaces';
+import Swal from 'sweetalert2';
+
+
+declare const gapi:any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   public formSubmitted = false;
 
   public loginForm = this.fb.group({
-    email: [localStorage.getItem('email') || '', [Validators.required, Validators.email]],
+    email: [
+      localStorage.getItem('email') || '',
+      [Validators.required, Validators.email],
+    ],
     password: ['', [Validators.required]],
     remember: [false],
   });
@@ -23,19 +28,43 @@ export class LoginComponent {
     private fb: FormBuilder,
     private usuarioService: UsuarioService
   ) {}
+  ngOnInit():void{
+    this.renderButton();
+  }
 
   login() {
-    this.usuarioService.login(this.loginForm.value)
-        .subscribe( resp => {
-          if( this.loginForm.get('remember').value){
-            localStorage.setItem('email', this.loginForm.get('email').value);
-          }else{
-            localStorage.removeItem('email');
-          }
-          Swal.fire(`Bienvenido`, '' , 'success')
-        }, (err) => {
-          Swal.fire('Error', err.error.msg,'error')
-        })
-      //  this.router.navigateByUrl('/');
+    this.usuarioService.login(this.loginForm.value).subscribe(
+      (resp) => {
+        if (this.loginForm.get('remember').value) {
+          localStorage.setItem('email', this.loginForm.get('email').value);
+        } else {
+          localStorage.removeItem('email');
+        }
+        Swal.fire(`Bienvenido`, '', 'success');
+      },
+      (err) => {
+        Swal.fire('Error', err.error.msg, 'error');
+      }
+    );
+    //  this.router.navigateByUrl('/');
+  }
+  onSuccess(googleUser) {
+   let id_token = googleUser.getAuthResponse().id_token;
+   console.log(id_token);
+  }
+  onFailure(error) {
+    console.log(error);
+  }
+
+  renderButton() {
+    gapi.signin2.render('my-signin2', {
+      scope: 'profile email',
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: 'dark',
+      onsuccess: this.onSuccess,
+      onfailure: this.onFailure
+    });
   }
 }
