74bd3a658ac3ff71a6b4a045378e70fc39d317f9
Joel Guerrero <joelg1014@hotmail.com>
2020-12-27 14:15:27 -0800

    271. Probando Strings

--- pruebas/src/app/basicas/string/string.spec.ts
+++ pruebas/src/app/basicas/string/string.spec.ts
@@ -1,4 +1,7 @@
-describe('Pruebas de Strings');
-
-it('Debe de regresar un string');
-it('Debe de contener un nombre');
+import { mensaje } from './string';
+describe('Pruebas de strings', () => {
+    it( 'Debe de regresar un string', () => {
+       const respuesta = mensaje('Joel');
+       expect( typeof respuesta ).toBe('string');
+    });
+});
