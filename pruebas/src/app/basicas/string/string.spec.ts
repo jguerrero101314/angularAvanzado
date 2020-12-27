20131bc7a1a3c2a21b5d9b3fd27ea352e8ed26a5
Joel Guerrero <joelg1014@hotmail.com>
2020-12-27 14:24:57 -0800

    272. Comprobar que el resultando CONTENGA algo

--- pruebas/src/app/basicas/string/string.spec.ts
+++ pruebas/src/app/basicas/string/string.spec.ts
@@ -1,7 +1,12 @@
 import { mensaje } from './string';
 describe('Pruebas de strings', () => {
     it( 'Debe de regresar un string', () => {
-       const respuesta = mensaje('Joel');
-       expect( typeof respuesta ).toBe('string');
+       const resp = mensaje('Joel');
+       expect( typeof resp ).toBe('string');
     });
+    it( 'Debe de retornar un saludo con el nombre enviado', () => {
+        const nombre = 'Joel';
+        const resp = mensaje(nombre);
+        expect( resp ).toContain(nombre);
+     });
 });
