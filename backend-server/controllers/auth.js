61edec24061f2dd225e83497bb515993c2bd1885
Joel Guerrero <joelg1014@hotmail.com>
2021-01-17 09:46:21 -0800

    181. Continuación de proyecto - AdminPro Frontend y Backend

--- backend-server/controllers/auth.js
+++ backend-server/controllers/auth.js
@@ -39,68 +39,72 @@ const login = async(req, res = response) => {
 };
 
 
-const googleSignIn = async(req, res = response) => {
+// const googleSignIn = async(req, res = response) => {
 
-    const googleToken = req.body.token;
+//     const googleToken = req.body.token;
+//     try {
 
 
-    try {
+//         const { name, email, picture } = await googleVerify(googleToken);
+//         console.log(name, email, picture);
 
-        const { name, email, picture } = await googleVerify(googleToken);
-        console.log(name, email, picture);
+//         const usuarioDB = await Usuario.findOne({ email });
+//         let usuario;
 
-        const usuarioDB = await Usuario.findOne({ email });
-        let usuario;
 
-        if (!usuarioDB) {
-            // si no existe el usuario
-            usuario = new Usuario({
-                nombre: name,
-                email,
-                password: '@@@',
-                img: picture,
-                google: true
-            });
-        } else {
-            // existe usuario
-            usuario = usuarioDB;
-            usuario.google = true;
-        }
+//         if (!usuarioDB) {
+//             // si no existe el usuario
+//             usuario = new Usuario({
+//                 nombre: name,
+//                 email,
+//                 password: '@@@',
+//                 img: picture,
+//                 google: true
+//             });
+//         } else {
+//             // existe usuario
+//             usuario = usuarioDB;
+//             usuario.google = true;
+//         }
 
-        // Guardar en DB
-        await usuario.save();
+//         // Guardar en DB
+//         await usuario.save();
 
-        // Generar el TOKEN - JWT
-        const token = await generarJWT(usuario.id);
-        console.log(token);
+//         // Generar el TOKEN - JWT
+//         const token = await generarJWT(usuario.id);
+//         console.log(token);
 
-        res.json({
-            ok: true,
-            token
-        });
+//         res.json({
+//             ok: true,
+//             token
+//         });
 
-    } catch (error) {
+//     } catch (error) {
 
-        res.status(401).json({
-            ok: false,
-            msg: 'Token no es correcto',
-        });
-    }
-}
+//         res.status(401).json({
+//             ok: false,
+//             msg: 'Token no es correcto',
+//         });
+//     }
+// }
 const renewToken = async(req, res = response) => {
 
     const uid = req.uid;
+    const usuario = await Usuario.findById(uid);
+    console.log(usuario);
 
     // Generar el TOKEN - JWT
     const token = await generarJWT(uid);
     res.json({
         ok: true,
-        uid
+        uid,
+        token,
+        usuario
     });
 }
 
 module.exports = {
     login,
-    googleSignIn,
+    //googleSignIn,
     renewToken
 }
--- backend-server/routes/auth.js
+++ backend-server/routes/auth.js
@@ -2,7 +2,7 @@
  * Path: '/api/login'
  */
 const { Router } = require('express');
-const { login, googleSignIn, renewToken } = require('../controllers/auth');
+const { login, renewToken } = require('../controllers/auth'); //googleSignIn,
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt')
@@ -18,12 +18,72 @@ router.post('/',
     login
 )
 
-router.post('/google', [
-        check('token', 'El token de Google es obligatorio').not().isEmpty(),
-        validarCampos
-    ],
-    googleSignIn
-);
+router.post('/google', async(req, res) => {
+    var token = req.body.token;
+    var googleUser = await verify(token)
+        .catch(() => {
+            return;
+        });
+
+    if (googleUser === undefined) {
+        return res.status(403).json({
+            ok: false,
+            mensaje: 'Token no válido'
+        });
+    } else {
+        Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar usuarios',
+                    errors: err
+                });
+            }
+            if (usuarioDB) {
+                if (usuarioDB.google === false) {
+                    return res.status(400).json({
+                        ok: false,
+                        mensaje: 'Debe usar su autenticación estándar',
+                    });
+                } else {
+                    var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+                    return res.status(200).json({
+                        ok: true,
+                        usuario: usuarioDB,
+                        token,
+                        id: usuarioDB.id
+                    });
+                }
+            } else {
+                // El usuario no existe, hay que crearlo
+                var usuario = new Usuario({
+                    nombre: googleUser.nombre,
+                    email: googleUser.email,
+                    img: googleUser.img,
+                    google: true,
+                    password: ':)'
+                });
+                usuario.save((err, usuarioDB) => {
+                    if (err) {
+                        return res.status(500).json({
+                            ok: false,
+                            mensaje: 'Error al guardar usuario',
+                            errors: err
+                        });
+                    }
+                    var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+                    return res.status(200).json({
+                        ok: true,
+                        usuario: usuarioDB,
+                        token,
+                        id: usuarioDB.id
+                    });
+                });
+            }
+        });
+    }
+});
+
 router.get('/renew', [
         validarJWT,
         validarCampos
