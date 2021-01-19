e71b3fe55b935431ac34346605712cc60bc59439
Joel Guerrero <joelg1014@hotmail.com>
2021-01-18 21:21:58 -0800

    290. Espías: returnValue

--- pruebas/src/app/intermedio/espias/medicos.component.spec.ts
+++ pruebas/src/app/intermedio/espias/medicos.component.spec.ts
@@ -1,35 +1,38 @@
 import { from, Observable, empty } from 'rxjs';
 import { MedicosComponent } from './medicos.component';
 import { MedicosService } from './MedicosService';
- 
- 
-describe('MedicosComponent', () => {
- 
-    let componente: MedicosComponent;
-    const servicio = new MedicosService(null as any);
- 
-    beforeEach( () => {
-        componente = new MedicosComponent(servicio);
-    });
- 
- 
-    it('Init: Debe cargar los medicos', () => {
-        const medicos = ['medico1', 'medico2', 'medico3'];
- 
-        spyOn(servicio, 'getMedicos').and.callFake(()=>{
-            return from([medicos]);
-        })
- 
-        componente.ngOnInit();
- 
-        expect(componente.medicos.length).toBeGreaterThan(0);
-    });
-    it('Debe de llamar al servidor para agregar un medico', () => {
 
-          const espia =  spyOn(servicio, 'agregarMedico').and.callFake( medico => {
-                return empty();
-            });
-            componente.agregarMedico();
-            expect(espia).toHaveBeenCalled();
+describe('MedicosComponent', () => {
+  let componente: MedicosComponent;
+  const servicio = new MedicosService(null as any);
+
+  beforeEach(() => {
+    componente = new MedicosComponent(servicio);
+  });
+
+  it('Init: Debe cargar los medicos', () => {
+    const medicos = ['medico1', 'medico2', 'medico3'];
+
+    spyOn(servicio, 'getMedicos').and.callFake(() => {
+      return from([medicos]);
     });
+
+    componente.ngOnInit();
+
+    expect(componente.medicos.length).toBeGreaterThan(0);
+  });
+  it('Debe de llamar al servidor para agregar un medico', () => {
+    const espia = spyOn(servicio, 'agregarMedico').and.callFake((medico) => {
+      return empty();
+    });
+    componente.agregarMedico();
+    expect(espia).toHaveBeenCalled();
+  });
+  it('Debe de agregar un nuevo medico al arreglo de medicos', () => {
+    const medico = { id: 1, nombre: 'Juan' };
+    spyOn(servicio, 'agregarMedico').and.returnValue(from([medico]));
+    componente.agregarMedico();
+    // expect(componente.medicos.length).toBe(1);
+    expect(componente.medicos.indexOf(medico)).toBeGreaterThanOrEqual(0);
+  });
 });
