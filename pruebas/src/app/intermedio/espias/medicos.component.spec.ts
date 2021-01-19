d4864e0fd762dcc65cbb9e6ec02ff766a72953f5
Joel Guerrero <joelg1014@hotmail.com>
2021-01-18 22:35:11 -0800

    292. Simular confirmaciones de usuario

--- pruebas/src/app/intermedio/espias/medicos.component.spec.ts
+++ pruebas/src/app/intermedio/espias/medicos.component.spec.ts
@@ -45,4 +45,24 @@ describe('MedicosComponent', () => {
     componente.agregarMedico();
     expect(componente.mensajeError).toBe(miError);
   });
+
+  it('Debe de llamar al servidor para borrar un medico', () => {
+   
+    spyOn(window,'confirm').and.returnValue(true);
+    const espia = spyOn(servicio,'borrarMedico').and.returnValue(empty());
+    componente.borrarMedico('1');
+
+    expect(espia).toHaveBeenCalledWith('1');
+
+  });
+
+  it('No Debe de llamar al servidor para borrar un medico', () => {
+   
+    spyOn(window,'confirm').and.returnValue(false);
+    const espia = spyOn(servicio,'borrarMedico').and.returnValue(empty());
+    componente.borrarMedico('1');
+
+    expect(espia).not.toHaveBeenCalledWith('1');
+
+  });
 });
