b9b0518ba9e6692a4a6734ef60310937740225ac
Joel Guerrero <joelg1014@hotmail.com>
2021-01-21 08:46:25 -0800

    298. Servicios en el TestingModule - Uso de servicios

--- pruebas/src/app/intermedio2/medico/medico.component.spec.ts
+++ pruebas/src/app/intermedio2/medico/medico.component.spec.ts
@@ -1,18 +1,28 @@
-import { MedicosComponent } from '../../intermedio/espias/medicos.component';
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed, ComponentFixture } from '@angular/core/testing'
 import { MedicoComponent } from './medico.component';
+import { MedicoService } from './medico.service';
 describe('Medico Component', () => {
     let component: MedicoComponent;
     let fixture: ComponentFixture<MedicoComponent>;
 
     beforeEach( () => {
         TestBed.configureTestingModule({
-            declarations: [MedicoComponent]
+            declarations: [MedicoComponent],
+            providers:[MedicoService],
+            imports:[HttpClientModule]
         })
         fixture = TestBed.createComponent( MedicoComponent );
-        component = fixture.componentInstance;
-       
+        component = fixture.componentInstance;    
     });
 
-    
+    it('Debe de crearse el componente', () => {
+        expect( component ).toBeTruthy();
+    });
+    it('Debe de retornar el nombre del medico', () => {
+        const nombre = 'Joel';
+        const  res = component.saludarMedico( nombre );
+
+        expect(res).toContain(nombre);
+    });
 });
--- pruebas/src/app/intermedio2/medico/medico.component.ts
+++ pruebas/src/app/intermedio2/medico/medico.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MedicoService } from './medico.service';
 
 @Component({
   selector: 'app-medico',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoComponent implements OnInit {
 
-  constructor() { }
+  medicos:any[] = [];
+
+  constructor( public _medicoService:MedicoService) { }
 
   ngOnInit(): void {
+  }
+
+  saludarMedico( nombre:string ){
+    return `Hola ${ nombre }`;
+  }
+
+  obtenerMedicos(){
+    this._medicoService.getMedicos().subscribe( (medicos:any[]) => this.medicos = medicos)
   }
 
 }
