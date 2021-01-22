aedb8195d29b9dc23c8257b106c71bcc6f43d5d0
Joel Guerrero <joelg1014@hotmail.com>
2021-01-22 08:29:41 -0800

    305. Verificar cambios en un elemento html tras eventos

--- pruebas/src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ pruebas/src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -46,8 +46,16 @@ describe('Incremendator Component', () => {
         botones[0].triggerEventHandler('click', null);
         expect(component.progreso).toBe(45);
         botones[1].triggerEventHandler('click', null);
-        expect(component.progreso).toBe(50);
-       
+        expect(component.progreso).toBe(50);  
+    });
+
+    it('En el titulo del componente, debe de mostrar el progreso', () => {
+        const botones = fixture.debugElement.queryAll( By.css('.btn-primary'));
+        botones[0].triggerEventHandler('click', null);
+        fixture.detectChanges();
+        const elem: HTMLElement = fixture.debugElement.query( By.css('h3') ).nativeElement;
+
+        expect( elem.innerHTML ).toContain('45');
     });
 
 });
