ddd53a5fc37a2c0a1a0883618769476e51317d0e
jguerrero101314 <joelg1014@hotmail.com>
2021-06-04 15:53:00 -0500

    187. Actualizar el perfil del usuario - continuacin

--- adminpro/src/app/pages/perfil/perfil.component.ts
+++ adminpro/src/app/pages/perfil/perfil.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from './../../models/usuario.model';
 
 @Component({
   selector: 'app-perfil',
@@ -9,16 +10,19 @@ import { UsuarioService } from '../../services/usuario.service';
 })
 export class PerfilComponent implements OnInit {
   public perfilForm: FormGroup;
+  public usuario: Usuario;
 
   constructor(
     private readonly fb: FormBuilder,
     private readonly usuarioService: UsuarioService
-  ) {}
+  ) {
+    this.usuario = usuarioService.usuario;
+  }
 
   ngOnInit(): void {
     this.perfilForm = this.fb.group({
-      nombre: ['123', [Validators.required]],
-      email: ['abc', [Validators.required, Validators.email]],
+      nombre: [this.usuario.nombre, [Validators.required]],
+      email: [this.usuario.email, [Validators.required, Validators.email]],
     });
   }
 
@@ -27,7 +31,9 @@ export class PerfilComponent implements OnInit {
     this.usuarioService
       .updateProfile(this.perfilForm.value)
       .subscribe((resp) => {
-        console.log(resp);
+        const { nombre, email } = this.perfilForm.value;
+        this.usuario.nombre = nombre;
+        this.usuario.email = email;
       });
   }
 }
