43c783d44220c61345077b4623a2cc3ca064d64e
jguerrero101314 <joelg1014@hotmail.com>
2021-06-07 09:50:35 -0500

    207. Modal para la carga de imágenes

---
+++ adminpro/src/app/components/modal-imagen/modal-imagen.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-imagen',
+  templateUrl: './modal-imagen.component.html',
+  styleUrls: ['./modal-imagen.component.css'],
+})
+export class ModalImagenComponent implements OnInit {
+  public ocultarModal: boolean;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  cerrarModal() {
+    this.ocultarModal = true;
+  }
+}
