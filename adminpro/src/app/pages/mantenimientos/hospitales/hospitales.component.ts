6aec83d8a0c2581e32377fa751f5c3f949383d9c
jguerrero101314 <joelg1014@hotmail.com>
2021-06-27 14:27:34 -0500

    217. Mostrar los hospitales en el HTML

--- adminpro/src/app/pages/mantenimientos/hospitales/hospitales.component.ts
+++ adminpro/src/app/pages/mantenimientos/hospitales/hospitales.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hospital } from '../../../models/hospital.model';
 import { HospitalService } from '../../../services/hospital.service';
 
 @Component({
@@ -7,11 +8,18 @@ import { HospitalService } from '../../../services/hospital.service';
   styleUrls: ['./hospitales.component.css'],
 })
 export class HospitalesComponent implements OnInit {
+  public hospitales: Hospital[] = [];
+  public cargando: boolean = true;
   constructor(private readonly hospitalService: HospitalService) {}
 
   ngOnInit(): void {
+    this.cargarHospital();
+  }
+
+  cargarHospital() {
     this.hospitalService.cargarHospitales().subscribe((hospitales) => {
-      console.log(hospitales);
+      this.hospitales = hospitales;
+      this.cargando = false;
     });
   }
 }
