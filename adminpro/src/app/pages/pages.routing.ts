8901aba5eb9b14b15bfbf2c2675c952687073a73
jguerrero101314 <joelg1014@hotmail.com>
2021-06-27 13:37:40 -0500

    215. Diseo de la pantalla de hospitales

--- adminpro/src/app/pages/pages.routing.ts
+++ adminpro/src/app/pages/pages.routing.ts
@@ -4,6 +4,8 @@ import { AuthGuard } from '../guards/auth.guard';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
+import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
+import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
 import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
 import { PagesComponent } from './pages.component';
 import { PerfilComponent } from './perfil/perfil.component';
@@ -53,7 +55,17 @@ const routes: Routes = [
       {
         path: 'usuarios',
         component: UsuariosComponent,
-        data: { titulo: 'Usuarios de aplicacion' },
+        data: { titulo: 'Mantenimiento de usuarios' },
+      },
+      {
+        path: 'hospitales',
+        component: HospitalesComponent,
+        data: { titulo: 'Mantenimiento de hospitales' },
+      },
+      {
+        path: 'medicos',
+        component: MedicosComponent,
+        data: { titulo: 'Mantenimiento de medicos' },
       },
     ],
   },
