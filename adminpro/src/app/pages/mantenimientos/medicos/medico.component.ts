0b6b6a9b02107fb1a8049990d9b9339604505e27
jguerrero101314 <joelg1014@hotmail.com>
2021-06-30 16:13:02 -0500

    227. Preparar el formulario de creación de médicos

--- adminpro/src/app/pages/mantenimientos/medicos/medico.component.ts
+++ adminpro/src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Hospital } from '../../../models/hospital.model';
+import { HospitalService } from './../../../services/hospital.service';
 
 @Component({
   selector: 'app-medico',
   templateUrl: './medico.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class MedicoComponent implements OnInit {
-
-  constructor() { }
+  public medicoForm: FormGroup;
+  public hospitales: Hospital[] = [];
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly hospitalService: HospitalService
+  ) {}
 
   ngOnInit(): void {
+    this.medicoForm = this.fb.group({
+      nombre: ['', [Validators.required]],
+      hospital: ['', [Validators.required]],
+    });
+    this.cargarHospitales();
   }
 
+  cargarHospitales() {
+    this.hospitalService
+      .cargarHospitales()
+      .subscribe((hospitales: Hospital[]) => {
+        this.hospitales = hospitales;
+        console.log(hospitales);
+      });
+  }
+
+  guardarMedico() {}
 }
