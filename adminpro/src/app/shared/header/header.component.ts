c125ed288788c46ad13ad8554b6878cd6416a5e7
jguerrero101314 <joelg1014@hotmail.com>
2021-07-02 20:46:11 -0500

    237. Continuación de proyecto - Búsqueda Global

--- adminpro/src/app/shared/header/header.component.ts
+++ adminpro/src/app/shared/header/header.component.ts
@@ -1,24 +1,21 @@
 import { Component } from '@angular/core';
-import { UsuarioService } from '../../services/usuario.service';
 import { Usuario } from '../../models/usuario.model';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeaderComponent {
-
   public usuario: Usuario;
 
-  constructor( private usuarioService: UsuarioService ) {
+  constructor(private usuarioService: UsuarioService) {
     this.usuario = usuarioService.usuario;
   }
 
   logout() {
     this.usuarioService.logout();
   }
-
-
+  buscar(txtTermino: string) {}
 }
--- adminpro/src/app/shared/shared.module.ts
+++ adminpro/src/app/shared/shared.module.ts
@@ -1,27 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
-
-
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-  ],
-  exports: [
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ]
+  declarations: [BreadcrumbsComponent, SidebarComponent, HeaderComponent],
+  exports: [BreadcrumbsComponent, SidebarComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, FormsModule],
 })
-export class SharedModule { }
+export class SharedModule {}
