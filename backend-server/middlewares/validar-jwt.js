457fbcfa17f8053cfca54158ec9c7941dc0e09f6
jguerrero101314 <joelg1014@hotmail.com>
2021-07-04 14:39:43 -0500

    243. Validar el ADMIN_ROLE en nuestro backend

--- backend-server/middlewares/validar-jwt.js
+++ backend-server/middlewares/validar-jwt.js
@@ -1,36 +1,61 @@
-const jwt = require('jsonwebtoken');
-
-
+const jwt = require("jsonwebtoken");
+const Usuario = require("../models/usuario");
 
 const validarJWT = (req, res, next) => {
+  // Leer el Token
+  const token = req.header("x-token");
 
-    // Leer el Token
-    const token = req.header('x-token');
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      msg: "No hay token en la petición",
+    });
+  }
 
-    if ( !token ) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'No hay token en la petición'
-        });
+  try {
+    const { uid } = jwt.verify(token, process.env.JWT_SECRET);
+    req.uid = uid;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Token no válido",
+    });
+  }
+};
+
+const varlidarADMIN_ROLE = async (req, res, next) => {
+  const uid = req.uid;
+
+  try {
+    const usuarioDB = await Usuario.findById(uid);
+
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Usuario no existe",
+      });
     }
 
-    try {
-        
-        const { uid } = jwt.verify( token, process.env.JWT_SECRET );
-        req.uid = uid;
-
-        next();
-
-    } catch (error) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'Token no válido'
-        });
+    if (usuarioDB.role !== "ADMIN_ROLE") {
+      return res.status(403).json({
+        ok: false,
+        msg: "No tiene privilegios para hacer eso",
+      });
     }
- 
-}
 
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
+};
 
 module.exports = {
-    validarJWT
-}
+  validarJWT,
+  varlidarADMIN_ROLE,
+};
