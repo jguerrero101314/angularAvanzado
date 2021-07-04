4c03ee434880de42ddc219d90022fcfe8e719ccb
jguerrero101314 <joelg1014@hotmail.com>
2021-07-04 16:34:20 -0500

    249. LazyLoad

--- adminpro/src/app/guards/auth.guard.ts
+++ adminpro/src/app/guards/auth.guard.ts
@@ -1,28 +1,48 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { UsuarioService } from '../services/usuario.service';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { UsuarioService } from '../services/usuario.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
 
-  constructor( private usuarioService: UsuarioService,
-               private router: Router) {}
-
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot) {
-
-      return this.usuarioService.validarToken()
-        .pipe(
-          tap( estaAutenticado =>  {
-            if ( !estaAutenticado ) {
-              this.router.navigateByUrl('/login');
-            }
-          })
-        );
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.usuarioService.validarToken().pipe(
+      tap((estaAutenticado) => {
+        if (!estaAutenticado) {
+          this.router.navigateByUrl('/login');
+        }
+      })
+    );
   }
-  
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.usuarioService.validarToken().pipe(
+      tap((estaAutenticado) => {
+        if (!estaAutenticado) {
+          this.router.navigateByUrl('/login');
+        }
+      })
+    );
+  }
 }
